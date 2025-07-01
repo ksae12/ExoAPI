0c6be83e04d97bdbc6532817be570f9be5b6d3a6
ksae12 <fernandoalmeid45@gmail.com>
2025-07-01 18:54:50 -0300

    feat: add autenticação e autorização com JWT token

--- Program.cs
+++ Program.cs
@@ -1,11 +1,33 @@
 using Exo.WebApi.Contexts;
 using Exo.WebApi.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<ExoContext, ExoContext>();
 builder.Services.AddControllers();
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = "JwtBearer";
+    options.DefaultChallengeScheme = "JwtBearer";
+}).AddJwtBearer("JwtBearer", options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("exoapi-chave-autenticacao")),
+        ClockSkew = TimeSpan.FromMinutes(45),
+        ValidIssuer = "exoapi.webapi",
+        ValidAudience = "exoapi.webapi"
+    };
+});
+
+
+
+
 builder.Services.AddTransient<ProjetoRepository, ProjetoRepository>();
 builder.Services.AddTransient<UsuarioRepository, UsuarioRepository>();
 
@@ -19,6 +41,8 @@ builder.Services.AddDbContext<ExoContext>(options =>
 var app = builder.Build();
 
 app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.UseEndpoints(endpoints =>
 {
--- obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
+++ obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73ac6742d1dccc79fe1d89a096f54ed021f05e54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2334114e60fe33b1b627ff7a0f361f2a1b3b437a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
